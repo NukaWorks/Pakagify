5bbaf2d07aacfca2f2a67ca0a4bbeee93b5d460e
Chabot Leonard <nukaworks@nuka.works>
2022-12-20 18:45:17 +0100

    :fire: Removed CreateWindow Override

--- Pakagify.Maui/App.xaml.cs
+++ Pakagify.Maui/App.xaml.cs
@@ -8,16 +8,5 @@ public partial class App : Application
 
 		MainPage = new MainPage();
 	}
-
-    protected override Window CreateWindow(IActivationState activationState)
-    {
-        var window = base.CreateWindow(activationState);
-        if (window != null)
-        {
-            window.Title = "Pakagify";
-        }
-
-        return window;
-    }
 }
 
