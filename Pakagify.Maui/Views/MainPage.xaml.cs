75aa7365057420cced62248728658e87ae4a8b8a
Chabot Leonard <nukaworks@nuka.works>
2022-12-20 19:31:55 +0100

    :sparkles: Added event for NewRepositoryBtn

--- Pakagify.Maui/Views/MainPage.xaml.cs
+++ Pakagify.Maui/Views/MainPage.xaml.cs
@@ -1,11 +1,17 @@
 ﻿namespace Pakagify.Maui;
 
+using System;
+
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-}
 
+    public void NewRepositoryBtn(Object sender, EventArgs e)
+    {
+		System.Diagnostics.Debug.WriteLine("Hello World !");
+	}
+}
 
