486283f8a4cee1244288c9c04f7d609ce4d21605
Chabot Leonard <nukaworks@nuka.works>
2022-12-20 16:02:38 +0100

    Code Reformat

--- Pakagify.Maui/App.xaml.cs
+++ Pakagify.Maui/App.xaml.cs
@@ -6,7 +6,18 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new MainPage();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        if (window != null)
+        {
+            window.Title = "Pakagify";
+        }
+
+        return window;
+    }
 }
 
