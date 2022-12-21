5aa342c13132577287edc23e7826b7a11a4844da
Chabot Leonard <nukaworks@nuka.works>
2022-12-21 16:06:35 +0100

    :sparkles: Code Reformat

--- src/Views/RecentView.jsx
+++ src/Views/RecentView.jsx
@@ -4,14 +4,14 @@ import { AppActivity, Button, UiApp } from '@powerws/uikit'
 export default function RecentView () {
   return (
     <AppActivity theme={'Light'}>
-      <div className={'App--SidePanel'}>
-        <Button size={'Medium'} color={'Primary'}>Nouveau Projet</Button>
+      <div className={'App__SidePanel'}>
+        <Button size={'Medium'} color={'Primary'} onClick={() => alert('Caca')}>Nouveau Projet</Button>
         <Button size={'Medium'}>Project Existant</Button>
       </div>
 
       <UiApp rounded>
-        <div className={'App--Header'}>
-        <h1 className={'App--Header__Title'}>Elements Récents</h1>
+        <div className={'App__Header'}>
+        <h1 className={'App__Header--Title'}>Elements Récents</h1>
       </div>
       </UiApp>
     </AppActivity>
