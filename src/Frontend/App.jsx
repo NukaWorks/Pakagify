a3bd39716a354eb837e8733416a918584a47b334
Chabot Leonard <nukaworks@nuka.works>
2022-12-25 13:41:52 +0100

    :sparkles: Impl handleToken

--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -14,20 +14,26 @@ import { useDetectClickOutside } from 'react-detect-click-outside'
 
 function GithubDialog ({ ...props }) {
   const context = React.useContext(DialogOverlayContext)
+  const inputRef = React.useRef(null)
+
+  const handleToken = () => {
+    console.log(inputRef.current.value)
+    closeDialogOverlay(context)
+  }
 
   return (
     <DialogOverlay name={'github-token'} {...props}>
       <FlexLayout direction={'Vertical'} spacing={15}>
         <Text size={13}>Create a Github Token</Text>
         <StackLayout spacing={5}>
-          <TextField placeholder={'Github Token'}/>
+          <TextField ref={inputRef} type={'password'} placeholder={'Github Token'} />
           <Button color={'Primary'}
                   onClick={() => window.open('https://github.com/settings/tokens/new?description=Pakagify&scopes=repo%2Cgist%2Cread%3Aorg%2Cworkflow')}>Generate</Button>
         </StackLayout>
 
         <FlexLayout justifyContent={'End'} direction={'Horizontal'} spacing={5}>
           <Button onClick={() => closeDialogOverlay(context)}>Cancel</Button>
-          <Button color={'Primary'}>OK</Button>
+          <Button color={'Primary'} onClick={handleToken}>OK</Button>
         </FlexLayout>
       </FlexLayout>
     </DialogOverlay>
