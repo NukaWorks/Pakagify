972cf46f3d22ea51325071e66bccac5f72008ced
powerm1nt <nukaworks@nuka.works>
2023-01-26 16:48:13 +0100

    :construction: Still in WIP state, repo fetching is stuck

--- src/Backend/Pakagify.js
+++ src/Backend/Pakagify.js
@@ -5,7 +5,11 @@ export class Pakagify extends EventEmitter {
   ghToken = ''
   fetchParams = {}
   user = {}
+  #reposData = new Map()
+  orgsData = new Map()
   isReady = false
+  isRepoBuilt = false
+
   constructor (token) {
     super()
     this.ghToken = token
@@ -19,7 +23,10 @@ export class Pakagify extends EventEmitter {
     this.#getUser().then(res => {
       this.user = res
       this.isReady = true
-      this.emit('ready', this)
+      this.buildRepoIndex().then(() => {
+        this.isRepoBuilt = true
+        this.emit('ready')
+      })
     })
   }
 
@@ -27,8 +34,51 @@ export class Pakagify extends EventEmitter {
     return (await fetch(`${this.baseUrl}/user`, this.fetchParams)).json()
   }
 
-  async getRepos () {
+  async getUserRepos () {
     if (this.isReady) return (await fetch(`${this.baseUrl}/users/${this.user.login}/repos`, this.fetchParams)).json()
     else throw new Error('Pakagify not ready yet.')
+  }
+
+  async getOrgRepos (name) {
+    if (this.isReady) return (await fetch(`${this.baseUrl}/orgs/${name}/repos`, this.fetchParams)).json()
+    else throw new Error('Pakagify not ready yet.')
+  }
+
+  getRepoData () {
+    return this.#reposData
+  }
+
+  async getOrgs () {
+    if (this.isReady) {
+      return await (await (fetch(`${this.baseUrl}/users/${this.user.login}/orgs`, this.fetchParams))).json().then(async orgs => {
+        orgs.forEach(async org => {
+          if (!this.orgsData.has(org.id)) this.orgsData.set(org.id, org)
+        })
+        return this.orgsData
+      })
+    } else throw new Error('Pakagify not ready yet.')
+  }
+
+  async buildRepoIndex () {
+    if (!this.isReady) {
+      throw new Error('Pakagify not ready yet.')
+    } else {
+      this.getOrgs().then(orgs => {
+        return orgs.forEach(async org => {
+          await this.getOrgRepos(org.login).then(async res => {
+            await res.forEach(repo => {
+              if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
+            })
+            // return this.#reposData
+          })
+        })
+      })
+
+      await this.getUserRepos().then(res => {
+        res.forEach(repo => {
+          if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
+        })
+      })
+    }
   }
 }
