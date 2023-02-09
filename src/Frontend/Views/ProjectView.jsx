1baa607c03c25a118f7518d1afdc541c6f141470
powerm1nt <nukaworks@nuka.works>
2023-02-09 16:55:43 +0100

    :construction: Impl Tabs

--- src/Frontend/Views/ProjectView.jsx
+++ src/Frontend/Views/ProjectView.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { AppActivity, UiApp } from '@powerws/uikit'
+import { AppActivity, StackLayout, Tab, TabList, TabPanel, Tabs, UiApp } from '@powerws/uikit'
 import Header from '../Common/Components/Header'
+import ProjectSettings from './Tabs/ProjectSettings'
+import Packages from './Tabs/Packages'
+import Artifacts from './Tabs/Artifacts'
 
 export default function ProjectView () {
   return (
@@ -8,7 +11,27 @@ export default function ProjectView () {
       <Header displayBackground={false} />
 
       <UiApp>
+        <StackLayout direction={'Vertical'}>
+          <Tabs>
+            <TabList>
+              <Tab>Artifacts</Tab>
+              <Tab>Packages</Tab>
+              <Tab>Settings</Tab>
+            </TabList>
 
+            <TabPanel>
+              <Artifacts />
+            </TabPanel>
+
+            <TabPanel>
+              <Packages />
+            </TabPanel>
+
+            <TabPanel>
+              <ProjectSettings />
+            </TabPanel>
+          </Tabs>
+        </StackLayout>
       </UiApp>
     </AppActivity>
   )
