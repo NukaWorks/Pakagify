a233ba26c466068f7d8bacb52746efbe98e74b3b
powerm1nt <nukaworks@nuka.works>
2023-02-16 21:02:43 +0100

    :sparkles: Impl auth command & ConfigProvider

--- src/Backend/Pakagify.cli.js
+++ src/Backend/Pakagify.cli.js
@@ -1,5 +1,9 @@
 import { program } from 'commander'
 import { version } from '../../package.json'
+import process from 'child_process'
+import { ConfigProvider } from './Common/ConfigProvider'
+
+const configProvider = new ConfigProvider()
 
 function mainCommand (argv) {
   program
@@ -7,9 +11,13 @@ function mainCommand (argv) {
     .description('Pakagify CLI')
     .version(version)
 
-  program.command('repository <name> <category>').description('Make a new component with story.')
-    .action((name, category) => {
-      // makeComponent(name, category)
+  program.command('auth <token>').description('Make a new component with story.')
+    .action(token => {
+      // process.env.PAKAGIFY_TOKEN = token
+      // console.log(process.env.PAKAGIFY_TOKEN)
+
+      configProvider.set('token', Buffer.from(token).toString('base64'))
+      configProvider.save()
     })
 
   program.command('package <name> <category>').description('Make a new story.')
