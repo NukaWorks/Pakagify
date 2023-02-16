1772768411318fbc193c093393b80ed7116a24c8
powerm1nt <nukaworks@nuka.works>
2023-02-16 21:02:27 +0100

    :sparkles: Added ConfigProvider

---
+++ src/Backend/Common/ConfigProvider.js
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import * as path from 'path'
+
+const CONFIG_DIR = path.join(process.env.HOME, '.config/pakagify/')
+
+class ConfigProvider {
+  constructor () {
+    this._config = {}
+  }
+
+  set (key, value) {
+    this._config[key] = value
+  }
+
+  get (key) {
+    return this._config[key]
+  }
+
+  has (key) {
+    return this._config.hasOwn(key, key)
+  }
+
+  remove (key) {
+    delete this._config[key]
+  }
+
+  clear () {
+    this._config = {}
+  }
+
+  save () {
+    // Save the json config
+    const configFile = path.join(CONFIG_DIR, 'pcli.json')
+    if (!fs.existsSync(CONFIG_DIR)) fs.mkdirSync(CONFIG_DIR, { recursive: true })
+    fs.writeFileSync(configFile, JSON.stringify(this._config, null, 2)) // TODO Impl locks
+  }
+}
+export { ConfigProvider }
