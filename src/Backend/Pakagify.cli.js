c665776a920f457c5adf534d422fa9fe46840941
powerm1nt <nukaworks@nuka.works>
2023-02-17 14:09:07 +0100

    :sparkles: auth command works

--- src/Backend/Pakagify.cli.js
+++ src/Backend/Pakagify.cli.js
@@ -1,8 +1,11 @@
 import { program } from 'commander'
 import { version } from '../../package.json'
 import { ConfigProvider } from './Common/ConfigProvider'
+import { Pakagify } from './Pakagify'
 
 const configProvider = new ConfigProvider()
+
+let pkInstance = null
 
 function mainCommand (argv) {
   program
@@ -10,7 +13,7 @@ function mainCommand (argv) {
     .description('Pakagify CLI')
     .version(version)
 
-  program.command('auth <token>').description('Make a new component with story.')
+  program.command('auth <token>').description('Authenticate with Github Token.')
     .action(token => {
       if (!token) {
         console.error('Token is required.')
@@ -18,16 +21,27 @@ function mainCommand (argv) {
       }
 
       configProvider.set('token', Buffer.from(token).toString('base64'))
-      configProvider.save()
+      pkInstance = new Pakagify(Buffer.from(token).toString('utf8'))
+      pkInstance.getUser().then(user => {
+        configProvider.save()
+        console.log(`Successfully authenticated as ${user.login} !`)
+      }).catch(err => {
+        console.error(err)
+        process.exit(1)
+      })
     })
 
-  program.command('add').description('Add a new repository, package ...')
+  program.command('add [command]').description('Add a new repository, package ...')
     .addCommand(new program.Command('repository <name>').description('Add a new repository.'))
     .addCommand(new program.Command('package <name>').description('Add a new package.'))
 
-  program.command('delete').description('Delete a repository, package ...')
+  program.command('delete [command]').description('Delete a repository, package ...')
     .addCommand(new program.Command('repository <name>').description('Delete a repository.'))
     .addCommand(new program.Command('package <name>').description('Delete a package.'))
+
+  program.command('list [command]').description('List a repository, package ...')
+    .addCommand(new program.Command('repository <name>').description('List a repository.'))
+    .addCommand(new program.Command('package <name>').description('List a package.'))
 
   program.parse(argv)
 }
