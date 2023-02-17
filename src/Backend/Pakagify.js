93b5cb9224cd147573d0ea5152b71d60c0450f36
powerm1nt <nukaworks@nuka.works>
2023-02-17 18:21:00 +0100

    :fire: Removed EventEmitter related code

--- src/Backend/Pakagify.js
+++ src/Backend/Pakagify.js
@@ -1,45 +1,29 @@
-import { EventEmitter } from 'events'
 import fetch from 'node-fetch'
 import { Octokit } from 'octokit'
 
-export class Pakagify extends EventEmitter {
+export class Pakagify {
   baseUrl = 'https://api.github.com'
   ghToken = ''
   fetchParams = {}
   user = {}
   #reposData = new Map()
   orgsData = new Map()
-  isReady = false
   isRepoBuilt = false
 
   constructor (token) {
-    super()
     this.ghToken = token
-
-    this.fetchParams = {
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    }
-
     this.octokit = new Octokit({ auth: token })
-
-    this.getUser().then(res => {
-      this.user = res
-      this.isReady = true
-      // this.buildRepoIndex().then(() => {
-      //   this.isRepoBuilt = true
-      //   this.emit('ready')
-      // })
-    })
   }
 
   async createRepo (user, name, isPrivate) {
-    if (user.login === await this.getUser()) {
-      return await this.octokit.rest.repos.createForAuthenticatedUser({ name, private: isPrivate })
-    } else {
-      return await this.octokit.rest.repos.createInOrg({ org: user, name, private: isPrivate })
-    }
+    return this.getUser().then(async res => {
+      console.log(user === res.login, user, res)
+      if (user === res.login) {
+        return await this.octokit.rest.repos.createForAuthenticatedUser({ name, private: isPrivate })
+      } else {
+        return await this.octokit.rest.repos.createInOrg({ org: user, name, private: isPrivate })
+      }
+    })
   }
 
   async getUser () {
@@ -48,13 +32,11 @@ export class Pakagify extends EventEmitter {
   }
 
   async getUserRepos () {
-    if (this.isReady) return (await fetch(`${this.baseUrl}/users/${this.user.login}/repos`, this.fetchParams)).json()
-    else throw new Error('Pakagify not ready yet.')
+    return (await fetch(`${this.baseUrl}/users/${this.user.login}/repos`, this.fetchParams)).json()
   }
 
   async getOrgRepos (name) {
-    if (this.isReady) return (await fetch(`${this.baseUrl}/orgs/${name}/repos`, this.fetchParams)).json()
-    else throw new Error('Pakagify not ready yet.')
+    return (await fetch(`${this.baseUrl}/orgs/${name}/repos`, this.fetchParams)).json()
   }
 
   getRepoData () {
@@ -62,36 +44,30 @@ export class Pakagify extends EventEmitter {
   }
 
   async getOrgs () {
-    if (this.isReady) {
-      return await (await (fetch(`${this.baseUrl}/users/${this.user.login}/orgs`, this.fetchParams))).json().then(async orgs => {
-        orgs.forEach(async org => {
-          if (!this.orgsData.has(org.id)) this.orgsData.set(org.id, org)
-        })
-        return this.orgsData
+    return await (await (fetch(`${this.baseUrl}/users/${this.user.login}/orgs`, this.fetchParams))).json().then(async orgs => {
+      orgs.forEach(async org => {
+        if (!this.orgsData.has(org.id)) this.orgsData.set(org.id, org)
       })
-    } else throw new Error('Pakagify not ready yet.')
+      return this.orgsData
+    })
   }
 
   async buildRepoIndex () {
-    if (!this.isReady) {
-      throw new Error('Pakagify not ready yet.')
-    } else {
-      this.getOrgs().then(orgs => {
-        return orgs.forEach(async org => {
-          await this.getOrgRepos(org.login).then(async res => {
-            await res.forEach(repo => {
-              if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
-            })
-            // return this.#reposData
+    this.getOrgs().then(orgs => {
+      return orgs.forEach(async org => {
+        await this.getOrgRepos(org.login).then(async res => {
+          await res.forEach(repo => {
+            if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
           })
+          // return this.#reposData
         })
       })
+    })
 
-      await this.getUserRepos().then(res => {
-        res.forEach(repo => {
-          if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
-        })
+    await this.getUserRepos().then(res => {
+      res.forEach(repo => {
+        if (!this.#reposData.has(repo.id)) this.#reposData.set(repo.id, repo)
       })
-    }
+    })
   }
 }
