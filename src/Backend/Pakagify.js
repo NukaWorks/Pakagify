2579f535cef38dc32b8dade963651426475c49a7
powerm1nt <nukaworks@nuka.works>
2023-02-18 23:17:15 +0100

    :art: Code Cleanup & Optimizations

--- src/Backend/Pakagify.js
+++ src/Backend/Pakagify.js
@@ -15,78 +15,87 @@ export class Pakagify {
 
   async createRelease (user, repoName) {
     const uuid = uuidv4().split('-')[1] // Generate & Get the first part of the uuid
-    return await this.#octokit.rest.repos.createRelease({ owner: user, repo: repoName, tag_name: uuid }).then(({ data }) => {
-      return data
-    })
+    return await this.#octokit.rest.repos.createRelease({ owner: user, repo: repoName, tag_name: uuid })
+      .then(({ data }) => {
+        return data
+      })
   }
 
   async pushRepoData (user, repoName, fileName, fileData) {
-    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName }).then(async ({ data }) => {
-      return await this.#octokit.rest.repos.uploadReleaseAsset({
-        owner: user,
-        repo: repoName,
-        release_id: data.id,
-        name: fileName,
-        data: fileData
+    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName })
+      .then(async ({ data }) => {
+        return await this.#octokit.rest.repos.uploadReleaseAsset({
+          owner: user,
+          repo: repoName,
+          release_id: data.id,
+          name: fileName,
+          data: fileData
+        })
       })
-    })
   }
 
   async deleteAsset (user, repoName, assetName) {
-    return this.#octokit.rest.repos.get({ owner: user, repo: repoName }).then(async ({ data }) => {
-      for (const asset of data.assets) {
-        if (asset.name === assetName) {
-          await this.#octokit.rest.repos.getReleaseAsset({
-            owner: user,
-            repo: repoName,
-            asset_id: asset.id
-          }).then(async (res) => {
-            return res
-          })
+    return this.#octokit.rest.repos.get({ owner: user, repo: repoName })
+      .then(async ({ data }) => {
+        for (const asset of data.assets) {
+          if (asset.name === assetName) {
+            await this.#octokit.rest.repos.getReleaseAsset({
+              owner: user,
+              repo: repoName,
+              asset_id: asset.id
+            }).then(async (res) => {
+              return res
+            })
+          }
         }
-      }
-    })
+      })
   }
 
   async getGitRepositoryData (user, repoName) {
-    return this.#octokit.rest.repos.get({ owner: user, repo: repoName }).then(async ({ data }) => {
-      return data
-    })
+    return this.#octokit.rest.repos.get({ owner: user, repo: repoName })
+      .then(async ({ data }) => {
+        return data
+      })
   }
 
   async getPakRepositoryData (user, repoName) {
-    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName }).then(async ({ data }) => {
-      for (const asset of data.assets) {
-        if (asset.name === 'repo.json') {
-          return await fetch(`https://api.github.com/repos/${user}/${repoName}/releases/assets/${asset.id}`, { // Get the asset data
-            headers: { Authorization: 'Bearer ' + this.#ghToken, Accept: 'application/octet-stream' }
-          }).then(res => res.json())
+    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName })
+      .then(async ({ data }) => {
+        for (const asset of data.assets) {
+          if (asset.name === 'repo.json') {
+          // Get the asset data
+            return await fetch(`https://api.github.com/repos/${user}/${repoName}/releases/assets/${asset.id}`, {
+              headers: { Authorization: 'Bearer ' + this.#ghToken, Accept: 'application/octet-stream' }
+            }).then(res => res.json())
+          }
         }
-      }
-    })
+      })
   }
 
   async deleteRelease (user, repoName) {
-    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName }).then(async rel => {
-      return await this.#octokit.rest.repos.deleteRelease({
-        owner: user,
-        repo: repoName,
-        release_id: rel.data.id
+    return this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName })
+      .then(async rel => {
+        return await this.#octokit.rest.repos.deleteRelease({
+          owner: user,
+          repo: repoName,
+          release_id: rel.data.id
+        })
       })
-    })
   }
 
   async getLatestRelease (user, repoName) {
-    return await this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName }).then(({ data }) => {
-      return data
-    })
+    return await this.#octokit.rest.repos.getLatestRelease({ owner: user, repo: repoName })
+      .then(({ data }) => {
+        return data
+      })
   }
 
   async getUser (refresh) {
     if (!this.#user || refresh) {
-      await this.#octokit.rest.users.getAuthenticated().then(({ data }) => {
-        this.#user = data
-      })
+      await this.#octokit.rest.users.getAuthenticated()
+        .then(({ data }) => {
+          this.#user = data
+        })
     }
 
     return this.#user
