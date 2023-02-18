db3943c00f19306e169a0acaf900b9531aae5620
powerm1nt <nukaworks@nuka.works>
2023-02-19 00:06:43 +0100

    :sparkles: Improved security

--- src/Backend/Common/Models/PackageModel.js
+++ src/Backend/Common/Models/PackageModel.js
@@ -9,6 +9,8 @@ export const PackageModel = {
   restart_required: false,
   last_updated: '',
   created_at: '',
+  install_location: '',
+  files: [{ name: '', relativePath: '', sha: '' }],
   scripts: {
     pre_inst: '',
     post_inst: ''
