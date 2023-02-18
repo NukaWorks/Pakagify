748f0cd8cd8e096e7d7c85e74e93d0b0babc838a
powerm1nt <nukaworks@nuka.works>
2023-02-18 23:01:33 +0100

    :art: Code Cleanup & Optimizations

--- src/Backend/Pakagify.cli.js
+++ src/Backend/Pakagify.cli.js
@@ -71,20 +71,22 @@ function mainCommand (argv) {
       }
 
       if (type === 'repository') {
-        processData(decodeToken(configProvider.get('token'))).getUser().then(user => {
+        processData(decodeToken(configProvider.get('token'))).getUser()
+          .then(user => {
           // Check if no org user specified
-          if (userAndName.length <= 1) {
-            userAndName[0] = user.login
-            userAndName[1] = name
-          }
+            if (userAndName.length <= 1) {
+              userAndName[0] = user.login
+              userAndName[1] = name
+            }
 
-          processData(decodeToken(configProvider.get('token'))).getPakRepositoryData(userAndName[0], userAndName[1]).then(res => {
-            console.log(res)
-          }).catch(err => {
-            console.error(err)
-            process.exit(1)
+            processData(decodeToken(configProvider.get('token'))).getPakRepositoryData(userAndName[0], userAndName[1])
+              .then(res => {
+                console.log(res)
+              }).catch(err => {
+                console.error(err)
+                process.exit(1)
+              })
           })
-        })
       }
     })
 
@@ -97,46 +99,56 @@ function mainCommand (argv) {
         process.exit(1)
       }
 
-      if (type === 'repository') {
-        // Check if no org user specified
-        if (userAndName.length <= 1) {
-          userAndName[0] = processData(decodeToken(configProvider.get('token'))).getUser().login
-          userAndName[1] = name
-        }
+      processData(decodeToken(configProvider.get('token'))).getUser()
+        .then(user => {
+          if (type === 'repository') {
+            // Check if no org user specified
+            if (userAndName.length <= 1) {
+              userAndName[0] = user.login
+              userAndName[1] = name
+            }
 
-        processData(decodeToken(configProvider.get('token'))).getGitRepositoryData(userAndName[0], userAndName[1]).then(repo => {
-          const repoModel = RepoModel
-          repoModel.name = repo.name
-          repoModel.description = repo.description
-          repoModel.author = repo.owner.login
-          repoModel.url = repo.html_url
-          repoModel.last_updated = repo.updated_at
-          repoModel.created_at = repo.created_at
-          repoModel.license = repo.license
+            processData(decodeToken(configProvider.get('token')))
+              .getGitRepositoryData(userAndName[0], userAndName[1])
+              .then(repo => {
+                const repoModel = RepoModel
+                repoModel.name = repo.name
+                repoModel.description = repo.description
+                repoModel.author = repo.owner.login
+                repoModel.url = repo.html_url
+                repoModel.last_updated = repo.updated_at
+                repoModel.created_at = repo.created_at
+                repoModel.license = repo.license
 
-          processData(decodeToken(configProvider.get('token'))).createRelease(userAndName[0], userAndName[1], true).then(res => {
-            processData(decodeToken(configProvider.get('token'))).pushRepoData(userAndName[0], userAndName[1], 'repo.json', JSON.stringify(RepoModel)).then(push => {
-              console.log(`Successfully created repository ${name} on ${res.tag_name} !`)
-              console.debug(push)
-            })
+                processData(decodeToken(configProvider.get('token')))
+                  .createRelease(userAndName[0], userAndName[1], true)
+                  .then(res => {
+                    processData(decodeToken(configProvider.get('token')))
+                      .pushRepoData(userAndName[0], userAndName[1], 'repo.json',
+                        JSON.stringify(RepoModel)).then(push => {
+                        console.log(`Successfully created repository ${name} on ${res.tag_name} !`)
+                        console.debug(push)
+                      })
 
-            console.debug(res)
-          })
+                    console.debug(res)
+                  })
 
-          console.debug(repo)
-        }).catch(err => {
-          console.error(err)
-          process.exit(1)
+                console.debug(repo)
+              }).catch(err => {
+                console.error(err)
+                process.exit(1)
+              })
+          } else if (type === 'package') {
+            // TODO
+          } else {
+            console.error('Invalid type.')
+            process.exit(1)
+          }
         })
-      } else if (type === 'package') {
-        // TODO
-      } else {
-        console.error('Invalid type.')
-        process.exit(1)
-      }
     })
 
-  program.command('delete <type> <name>').description('Delete a repository, package ...')
+  program.command('delete <type> <name>')
+    .description('Delete a repository, package ...')
     .option('-repo, --repository', 'Select the repository.')
     .action((type, name) => {
       const userAndName = name.split('/')
@@ -146,46 +158,56 @@ function mainCommand (argv) {
         console.error('You need to authenticate first.')
         process.exit(1)
       }
+      processData(decodeToken(configProvider.get('token'))).getUser()
+        .then(user => {
+          if (type === 'repository') {
+            // Check if no org user specified
+            if (userAndName.length <= 1) {
+              userAndName[0] = user.login
+              userAndName[1] = name
+            }
 
-      if (type === 'repository') {
-        // Check if no org user specified
-        if (userAndName.length <= 1) {
-          userAndName[0] = processData(decodeToken(configProvider.get('token'))).getUser().login
-          userAndName[1] = name
-        }
+            let latestReleaseTag = processData(decodeToken(configProvider.get('token')))
+              .getLatestRelease(userAndName[0], userAndName[1])
+              .then(rel => {
+                latestReleaseTag = rel.tag_name
+              })
 
-        let latestReleaseTag = processData(decodeToken(configProvider.get('token'))).getLatestRelease(userAndName[0], userAndName[1]).then(rel => {
-          latestReleaseTag = rel.tag_name
+            processData(decodeToken(configProvider.get('token')))
+              .deleteRelease(userAndName[0], userAndName[1], true)
+              .then(res => {
+                console.log(`Successfully deleted repository ${name} on ${latestReleaseTag} !`)
+                console.debug(res)
+              }).catch(err => {
+                console.error(err)
+                process.exit(1)
+              })
+          } else if (type === 'package') {
+            if (!configProvider.has('token')) {
+              console.error('You need to authenticate first.')
+              process.exit(1)
+            }
+
+            if (!options.repository) {
+              console.error('You need to specify the repository.')
+              process.exit(1)
+            }
+
+            processData(decodeToken(configProvider.get('token')))
+              .deleteRelease(userAndName[0], userAndName[1], true)
+              .then(res => {})
+          } else {
+            console.error('Invalid type.')
+            process.exit(1)
+          }
         })
-
-        processData(decodeToken(configProvider.get('token'))).deleteRelease(userAndName[0], userAndName[1], true).then(res => {
-          console.log(`Successfully deleted repository ${name} on ${latestReleaseTag} !`)
-          console.log(res)
-        }).catch(err => {
-          console.error(err)
-          process.exit(1)
-        })
-      } else if (type === 'package') {
-        if (!configProvider.has('token')) {
-          console.error('You need to authenticate first.')
-          process.exit(1)
-        }
-
-        if (!options.repository) {
-          console.error('You need to specify the repository.')
-          process.exit(1)
-        }
-
-        processData(decodeToken(configProvider.get('token'))).deleteRelease(userAndName[0], userAndName[1], true).then(res => {})
-      } else {
-        console.error('Invalid type.')
-        process.exit(1)
-      }
     })
 
-  program.command('list [command]').description('List a repository, package ...') // TODO Not working yet
+  program.command('list [command]')
+    .description('List a repository, package ...') // TODO Not working yet
     .action(() => {
-      program.addCommand(new program.Command('repository <name>').description('List a repository.'))
+      program.addCommand(new program.Command('repository <name>')
+        .description('List a repository.'))
         .action(name => {
           // processData(decodeToken(configProvider.get('token'))).getUserRepos()
         })
