42017d764574b4a054dda23243ebe8289291729c
powerm1nt <nukaworks@nuka.works>
2023-02-18 16:38:00 +0100

    :sparkles: Added PackageModel

---
+++ src/Backend/Common/Models/PackageModel.js
@@ -0,0 +1,15 @@
+
+export const PackageModel = {
+  name: '',
+  description: '',
+  author: '',
+  url: '',
+  arch: '',
+  platform: '',
+  last_updated: '',
+  created_at: '',
+  scripts: {
+    pre_inst: '',
+    post_inst: ''
+  }
+}
