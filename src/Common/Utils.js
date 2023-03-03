d6a6b2119782b6d23ccc3ce661167d24a52098aa
powerm1nt <nukaworks@nuka.works>
2023-03-03 18:38:41 +0100

    :sparkles: Added formatTime & calculateBitrate

--- src/Common/Utils.js
+++ src/Common/Utils.js
@@ -17,4 +17,37 @@ function listFilesRecursively (directory) {
   return fileList
 }
 
-export { listFilesRecursively }
+function calculateBitrate (bytesDownloaded, downloadTimeSeconds) {
+  // Calculate the bitrate in bytes per second
+  const bytesPerSecond = bytesDownloaded / downloadTimeSeconds
+
+  // Convert bytes per second to megabytes per second
+  const megabytesPerSecond = bytesPerSecond / (1024 * 1024)
+
+  // Choose the appropriate unit based on the bitrate value
+  let unit = 'B/s'
+  let bitrate = bytesPerSecond
+  if (megabytesPerSecond >= 1) {
+    unit = 'MB/s'
+    bitrate = megabytesPerSecond
+  } else if (bytesPerSecond >= 1024) {
+    unit = 'KB/s'
+    bitrate = bytesPerSecond / 1024
+  }
+
+  // Return the calculated bitrate with the appropriate unit, rounded to two decimal places
+  return `${bitrate.toFixed(2)} ${unit}`
+}
+
+function formatTime (seconds) {
+  const hours = Math.floor(seconds / 3600)
+  const minutes = Math.floor((seconds - (hours * 3600)) / 60)
+  const remainingSeconds = seconds - (hours * 3600) - (minutes * 60)
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds.toFixed(0).toString().padStart(2, '0')}`
+}
+
+export {
+  listFilesRecursively,
+  calculateBitrate,
+  formatTime
+}
