5829eeca0219c845fe5069c5c97324c893e8742d
Powerm1nt <leonard.chabot@nuka.works>
2024-12-18 01:04:20 +0100

    Tabs works but in controlled mode (UiKit issue)

--- src/PkDash/Views/ProjectView.tsx
+++ src/PkDash/Views/ProjectView.tsx
@@ -3,15 +3,17 @@ import { Header } from "../../Common/UiComponents/Header";
 import ProjectSettings from "./Tabs/ProjectSettings";
 import { Releases } from "./Tabs/Releases";
 import { Packages } from "./Tabs/Packages";
+import { useState } from "react";
 
 export default function ProjectView() {
+  const [tabSelected, setTabSelected] = useState(0);
   return (
     <AppActivity theme={"Light"} direction={"Vertical"}>
       <Header displayBackground={false} />
 
       <UiApp>
         <StackLayout direction={"Vertical"}>
-          <Tabs>
+          <Tabs selectedIndex={tabSelected} onSelect={(e) => setTabSelected(e)}>
             <TabList>
               <Tab id={"releases"}>Releases</Tab>
               <Tab id={"packages"}>Packages</Tab>
